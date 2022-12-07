8af90970cc22992da8d79b49923e35b3e1f3a242
Kevin Haller <contact@kevinhaller.dev>
2022-12-07 22:22:17 +0100

    #2: Removes the docker-compose for neo4j, and integrates the running and preparation of Neo4J instance into the application. Neo4J admin tool is now used to import data for a dataset. It is much!! faster than doing it over the query interface.

--- kgrec/cmd/compute.py
+++ kgrec/cmd/compute.py
@@ -1,9 +1,11 @@
+import time
+
 import typer
 
 from sys import stderr
-from neo4j import GraphDatabase
+
 from kgrec.datasets import Dataset
-from kgrec.metrics.initializer import Initializer
+from kgrec.metrics.initializer import LocalNeo4JInstance, GraphDB
 from kgrec.metrics.ldsd import LDSD
 
 app = typer.Typer()
@@ -13,53 +15,16 @@ app.add_typer(db, name='db')
 
 
 @app.command(name='ldsd', help='compute the LDSD metric')
-def compute_ldsd(neo4j_url: str = 'bolt://localhost:7687',
-                 neo4j_username: str = 'neo4j', neo4j_password: str = 'neo4j',
-                 no_auth: bool = False, model_out_directory: str = 'model',
-                 number_of_jobs: int = 1,
-                 dataset: str = 'pokemon'):
-    auth = (neo4j_username, neo4j_password) if not no_auth else None
-    driver = GraphDatabase.driver(neo4j_url, auth=auth)
-
+def compute_ldsd(dataset: str = 'pokemon', model_out_directory: str = 'model',
+                 number_of_jobs: int = 1):
     ds = Dataset.get_dataset_for(dataset)
     if ds is None:
         print('err: the given dataset "%s" isn\'t supported' % dataset,
               file=stderr)
         exit(1)
 
-    metric = LDSD(dataset=ds, neo4j_driver=driver, num_of_jobs=number_of_jobs)
-    metric.compute(model_out_directory)
-
-
-@db.command(name='init', help='initialize the DB with specified dataset')
-def init(dataset: str = 'pokemon', neo4j_url: str = 'bolt://localhost:7687',
-         neo4j_username: str = 'neo4j', neo4j_password: str = 'neo4j',
-         no_auth: bool = False):
-    auth = (neo4j_username, neo4j_password) if not no_auth else None
-    driver = GraphDatabase.driver(neo4j_url, auth=auth)
-
-    ds = Dataset.get_dataset_for(dataset)
-    if ds is None:
-        print('err: the given dataset "%s" isn\'t supported' % dataset,
-              file=stderr)
-        exit(1)
-
-    initializer = Initializer(dataset=ds, driver=driver)
-    initializer.load()
-
-
-@db.command(name='clear', help='remove all data from the DB')
-def clear(dataset: str = 'pokemon', neo4j_url: str = 'bolt://localhost:7687',
-          neo4j_username: str = 'neo4j', neo4j_password: str = 'neo4j',
-          no_auth: bool = False):
-    auth = (neo4j_username, neo4j_password) if not no_auth else None
-    driver = GraphDatabase.driver(neo4j_url, auth=auth)
-
-    ds = Dataset.get_dataset_for(dataset)
-    if ds is None:
-        print('err: the given dataset "%s" isn\'t supported' % dataset,
-              file=stderr)
-        exit(1)
-
-    initializer = Initializer(dataset=ds, driver=driver)
-    initializer.clear()
+    inst = LocalNeo4JInstance(working_dir_path='deployment/neo4j/%s' % ds.name)
+    with GraphDB(neo4j_instance=inst, dataset=ds) as graphDB:
+        metric = LDSD(dataset=ds, neo4j_driver=graphDB.driver,
+                      num_of_jobs=number_of_jobs)
+        metric.compute(model_out_directory)
--- kgrec/metrics/initializer.py
+++ kgrec/metrics/initializer.py
@@ -1,104 +1,196 @@
-import math
-from typing import Mapping, Sequence
+import csv
 
-import numpy as np
-import progressbar as pb
+from os import makedirs
+from os.path import join, exists
+from pathlib import Path
+from typing import Mapping, Sequence, Tuple
 
-from neo4j import Neo4jDriver, Record, Session
+from neo4j import GraphDatabase, Neo4jDriver
+
 from kgrec.datasets import Dataset
+from python_on_whales import docker
 
 
-class Initializer:
+class LocalNeo4JInstance:
+    """ a local instance of Neo4J """
+
+    version = '4.4.4'
+
+    def __init__(self, working_dir_path: str, mem_in_gb: int = 4):
+        """
+        creates a new local instance of Neo4J with the working directory at the
+        specified location.
+
+        :param working_dir_path: path to the working directory for this
+        instance.
+        :param mem_in_gb: memory dedicated to the Neo4J instance in GB.
+        """
+        self.pwd = working_dir_path
+        self._max_mem = mem_in_gb
+        self._container = None
+
+    def _get_volume_dir_paths(self) -> Mapping[str, str]:
+        """
+        gets a dictionary with the name of volumes as a key, and the path to it
+        on the host as a value.
+
+        :return: a dictionary with volume names as key, and corresponding path
+        as value.
+        """
+        return {
+            'conf': str(Path(join(self.pwd, 'conf')).absolute()),
+            'data': str(Path(join(self.pwd, 'data')).absolute().resolve()),
+            'logs': str(Path(join(self.pwd, 'logs')).absolute().resolve()),
+            'plugins': str(Path(join(self.pwd,
+                                     'plugins')).absolute().resolve()),
+            'import': str(Path(join(self.pwd, 'import')).absolute().resolve()),
+        }
+
+    def _get_volumes(self) -> Sequence[Tuple[str, str, str]]:
+        """ gets the volumes to mount """
+        vol_dict = self._get_volume_dir_paths()
+        return [
+            (vol_dict['data'], '/data', 'rw'),
+            (vol_dict['logs'], '/logs', 'rw'),
+            (vol_dict['plugins'], '/plugins', 'rw'),
+            (vol_dict['conf'], '/conf', 'rw'),
+            (vol_dict['import'], '/import', 'ro'),
+        ]
+
+    def create_volume_dirs(self):
+        """ creates the directories for the volumes which will be mounted to
+        this local Neo4J instance """
+        dir_paths = self._get_volume_dir_paths().values()
+        for d in dir_paths:
+            if not exists(d):
+                makedirs(d)
+
+    @property
+    def driver(self) -> Neo4jDriver:
+        """ gets the Neo4J driver to the local database instance """
+        return GraphDatabase.driver('bolt://127.0.0.1:7687',
+                                    auth=('neo4j', 'test'))
+
+    def construct_data_importer(self, dataset: Dataset):
+        """
+
+        :param dataset:
+        :return:
+        """
+        return DatasetImporter(neo4j_instance=self, dataset=dataset)
+
+    def run(self):
+        """ runs this local Neo4J instance """
+        mm = self._max_mem
+        ps = int(mm / 2)
+        ct = docker.run(image='neo4j:%s-community' % self.version,
+                        detach=True, remove=True,
+                        volumes=self._get_volumes(),
+                        publish=[
+                            ('127.0.0.1:7687', '7687'),
+                            ('127.0.0.1:7474', '7474'),
+                        ],
+                        envs={
+                            'NEO4J_dbms_memory_pagecache_size': '%dG' % ps,
+                            'NEO4J_dbms.memory.heap.initial_size': '%dG' % mm,
+                            'NEO4J_dbms_memory_heap_max_size': '%dG' % mm,
+                            'NEO4JLABS_PLUGINS': '["apoc"]',
+                        })
+        self._container = ct
+
+    def execute_load_command(self):
+        """ executes a load command on this Neo4J instance """
+        docker.run(image='neo4j:%s-community' % self.version, remove=True,
+                   volumes=self._get_volumes(),
+                   entrypoint='neo4j-admin',
+                   command=[
+                       'import',
+                       '--database', 'neo4j',
+                       '--nodes', '/import/entities.csv',
+                       '--relationships', '/import/statements.csv',
+                       '--force',
+                   ])
+
+    def stop(self):
+        """ stops this local Neo4J instance """
+        if self._container is not None:
+            docker.container.stop(self._container)
+
+
+class DatasetImporter:
+    """ an exporter of the dataset in a format that is understood by Neo4J """
+
+    def __init__(self, neo4j_instance: LocalNeo4JInstance, dataset: Dataset):
+        self._neo4j_instance = neo4j_instance
+        self._dataset = dataset
+
+    def load(self) -> bool:
+        """
+        loads the given statements into the database with the specified name.
+
+        :return: `True`, if data was imported, otherwise `False`.
+        """
+        self._neo4j_instance.create_volume_dirs()
+        pwd = self._neo4j_instance.pwd
+        lock_f = join(pwd, 'init.lock')
+        if not exists(lock_f):
+            import_dir = join(pwd, 'import')
+            self._write_entities(join(import_dir, 'entities.csv'))
+            self._write_statements(join(import_dir, 'statements.csv'))
+            self._neo4j_instance.execute_load_command()
+            with open(lock_f, 'w') as f:
+                print('ok', file=f)
+            return True
+        else:
+            return False
+
+    def _write_entities(self, entities_file_path: str):
+        """
+        writes the entities of the dataset to a CSV file that is understood by
+        Neo4J.
+
+        :param entities_file_path: path to the CSV file to which to write
+        entities.
+        """
+        with open(entities_file_path, 'w') as entities_csv_file:
+            writer = csv.writer(entities_csv_file, delimiter=',')
+            writer.writerow(['tsvID:ID', ':LABEL'])
+            cap_name = self._dataset.capitalized_name
+            for tsv_id in self._dataset.index.index.values:
+                writer.writerow([tsv_id, cap_name])
+
+    def _write_statements(self, statements_file_path: str):
+        """
+        writes the statements of the dataset to a CSV file that is understood by
+        Neo4J.
+
+        :param statements_file_path: path to the CSV file to which to write the
+        statements.
+        """
+        with open(statements_file_path, 'w') as statements_csv_file:
+            writer = csv.writer(statements_csv_file, delimiter=',')
+            writer.writerow([':START_ID', ':END_ID', ':TYPE'])
+            for _, row in self._dataset.statements.iterrows():
+                writer.writerow([row['subj'], row['obj'], 'P%d' % row['pred']])
+
+
+class GraphDB:
     """ Initializer for Neo4J, which loads the statements into the database """
 
-    def __init__(self, dataset: Dataset, driver: Neo4jDriver,
-                 batch_size: int = 100000):
+    def __init__(self, neo4j_instance: LocalNeo4JInstance, dataset: Dataset):
+        self._neo4j_instance = neo4j_instance
         self._dataset = dataset
-        self._driver = driver
-        self._batch_size = batch_size
 
-    def _create_entities(self, session: Session) -> Mapping[int, int]:
-        """ loads entities of the dataset into the Neo4J graph database """
-        ds_name = self._dataset.capitalized_name
-        print('Load entities:')
-        nodes = {}
-        size = len(self._dataset.index)
-        with pb.ProgressBar(max_value=size) as p:
-            n = 0
-            p.update(n)
-            chunks = np.array_split(self._dataset.index,
-                                    math.ceil(float(size) / self._batch_size))
-            for chunk in chunks:
-                n_d = session.write_transaction(self._write_nodes, ds_name,
-                                                [e for e in chunk.index])
-                for record in n_d:
-                    nodes[record[1]] = record[0]
+    def __enter__(self):
+        di = self._neo4j_instance.construct_data_importer(self._dataset)
+        di.load()
+        self._neo4j_instance.run()
+        return self
 
-                n += len(chunk)
-                p.update(n)
+    @property
+    def driver(self) -> Neo4jDriver:
+        """ gets the Neo4J driver to the database instance """
+        return self._neo4j_instance.driver
 
-            return nodes
-
-    def _create_statement(self, node_dict: Mapping[int, int], session: Session):
-        """ loads statements of the dataset into the Neo4J graph database """
-        ds_name = self._dataset.capitalized_name
-        print('Load statements:')
-        size = len(self._dataset.statements)
-        with pb.ProgressBar(max_value=size) as p:
-            n = 0
-            p.update(n)
-            chunks = np.array_split(self._dataset.statements,
-                                    math.ceil(float(size) / self._batch_size))
-            for chunk in chunks:
-                statements = ['[%d,\'p%d\',%d]' % (node_dict[int(row['subj'])],
-                                                   int(row['pred']),
-                                                   node_dict[int(row['obj'])])
-                              for i, row in chunk.iterrows()]
-                session.write_transaction(self._write_edges, ds_name,
-                                          statements)
-
-    def load(self):
-        """ loads the given statements into the database with the specified name
-        """
-        with self._driver.session() as session:
-            entity_map = self._create_entities(session)
-            self._create_statement(entity_map, session)
-
-    def clear(self):
-        """ deletes all the nodes and statements loaded into the Neo4J database
-        """
-        with self._driver.session() as session:
-            session.write_transaction(self._delete_all,
-                                      self._dataset.capitalized_name)
-
-    @staticmethod
-    def _delete_all(tx, label: str):
-        r = tx.run('''
-        MATCH (n:%s)
-        CALL {
-          WITH n
-          DETACH DELETE n
-        }
-        ''' % label)
-        return r.single()
-
-    @staticmethod
-    def _write_nodes(tx, label: str, chunk: Sequence[int]) -> Sequence[Record]:
-        result = tx.run(('''
-        UNWIND [%s] as node_id
-        CREATE (n:Resource:$label)
-        SET n.tsv_id = node_id
-        RETURN id(n) as node_id, n.tsv_id as tsv_id
-        ''' % ','.join(map(str, chunk))).replace('$label', label))
-        return result.values()
-
-    @staticmethod
-    def _write_edges(tx, label: str, statements: Sequence[str]):
-        tx.run(('''
-        UNWIND[%s] as stmt
-        CALL apoc.nodes.get([stmt[0]]) YIELD node as a
-        CALL apoc.nodes.get([stmt[2]]) YIELD node as b
-        CALL apoc.create.relationship(a, stmt[1], null, b) YIELD rel
-        RETURN rel
-        ''' % ','.join(statements)).replace('$label', label))
-        return None
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self._neo4j_instance.stop()
